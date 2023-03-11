6491dd6a8e0a12c1d498993da5f4f65aaefe74c0
Asis Johnson <asisjohnson@techexchange.in>
2023-03-11 20:41:58 +0000

    Updated backend.Backend.sign_up

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,4 +1,5 @@
 from google.cloud import storage
+import hashlib
 
 class Backend:
 
@@ -17,10 +18,22 @@ class Backend:
     def upload(self):
         pass
 
-    def sign_up(self):
-        pass
+    def sign_up(self, user, password): #Asis
+        bucket_name = "bt-wikiviewer-users_passwords"
+        
+        storage_client = storage.Client()
 
-    def sign_in(self):
+        blobs = storage_client.list_blobs(bucket_name)
+        if user not in blobs: 
+            bucket = storage_client.bucket(bucket_name)
+            blob = bucket.blob(user)
+
+            hashed = hashlib.sha256(password.encode()).hexdigest()
+
+            with blob.open("w") as f:
+                f.write(hashed)
+
+    def sign_in(self): #Asis
         pass
 
     def get_image(self):
